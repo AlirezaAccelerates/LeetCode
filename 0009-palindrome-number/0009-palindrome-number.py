8c9e467d000c79abcf272c4fec31271585e4ea6c
Alireza <96019816+AlirezaAccelerates@users.noreply.github.com>
2024-07-10 11:59:00 -0400

    Time: 33 ms (82.05%), Space: 11.6 MB (22.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        
         """
         if str(x) == str(x)[::-1]:
             return True
         else: 
             return False
         """
+        
         # Follow up: Could you solve it without converting the integer to a string?
         if x < 0:
             return False
