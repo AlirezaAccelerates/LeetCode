53cc12363af57c03bb05a3901cca27a5f6389763
Alireza <96019816+AlirezaAccelerates@users.noreply.github.com>
2024-07-30 20:53:47 -0400

    Time: 17 ms (47.26%), Space: 11.7 MB (5.21%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in reversed(range(len(digits))):
+
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+        
+        return [1] + digits
+
+        
