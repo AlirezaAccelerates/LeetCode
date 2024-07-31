4f82203662a50553bfbda7a4f22c99444253e847
Alireza <96019816+AlirezaAccelerates@users.noreply.github.com>
2024-07-30 20:53:59 -0400

    Time: 16 ms (51.59%), Space: 11.5 MB (62.21%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: List[int]
         """
         for i in reversed(range(len(digits))):
-
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-
             digits[i] = 0
-        
         return [1] + digits
 
         
